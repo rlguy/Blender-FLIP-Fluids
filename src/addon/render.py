540f1e7f6a142cca962020a041cc552fe59174e8
johnakki <rajGUxnI6Vpf2uiZ@protonmail.com>
2018-06-10 17:13:53 +0100

    Blender 2.8 changes

--- src/addon/render.py
+++ src/addon/render.py
@@ -54,7 +54,7 @@ def render_complete(scene):
     IS_FRAME_REQUIRING_RELOAD = True
 
 
-def scene_update_post(scene):
+def frame_change_post(scene):
     global IS_FRAME_REQUIRING_RELOAD
     if IS_FRAME_REQUIRING_RELOAD:
         IS_FRAME_REQUIRING_RELOAD = False
@@ -257,7 +257,7 @@ def __generate_icosphere():
         p.use_smooth = True
 
     obj = bpy.data.objects.new("Icosphere", mesh)
-    bpy.context.scene.objects.link(obj) 
+    bpy.context.layer_collection.collection.objects.link(obj) 
 
     return obj
 
